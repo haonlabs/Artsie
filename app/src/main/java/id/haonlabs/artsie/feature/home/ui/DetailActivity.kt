09599a8b1bb9bb0195acb0e4480c75e8ea1592f9
Haon <developedbyadifens@gmail.com>
2024-12-07 15:52:18 +0700

    💄 done detail page

--- app/src/main/java/id/haonlabs/artsie/feature/home/ui/DetailActivity.kt
+++ app/src/main/java/id/haonlabs/artsie/feature/home/ui/DetailActivity.kt
@@ -3,12 +3,11 @@ package id.haonlabs.artsie.feature.home.ui
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import id.haonlabs.artsie.R
 import id.haonlabs.artsie.feature.home.model.Art
 
@@ -17,12 +16,9 @@ class DetailActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
     setContentView(R.layout.activity_detail)
-    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-      val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-      v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-      insets
-    }
-
+    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    supportActionBar?.title = "Detail"
+    setTheme(R.style.Theme_Artsie)
     val data =
         if (Build.VERSION.SDK_INT >= 33) {
           intent.getParcelableExtra<Art>("DATA", Art::class.java)
@@ -33,15 +29,27 @@ class DetailActivity : AppCompatActivity() {
     Log.d("Detail Data", data.toString())
 
     val photo: ImageView = findViewById(R.id.iv_detail_photo)
-    val name: TextView = findViewById(R.id.tv_name)
-    val desc: TextView = findViewById(R.id.tv_desc)
-    val artist: TextView = findViewById(R.id.tv_artist)
+    val title: TextView = findViewById(R.id.tv_title)
+    val materials: TextView = findViewById(R.id.tv_materials)
+    val dimensions: TextView = findViewById(R.id.tv_dimensions)
+    val history: TextView = findViewById(R.id.tv_history)
 
     if (data != null) {
       photo.setImageResource(data.photo)
-      name.text = data.title
-      desc.text = data.history
-      artist.text = "Materials: ${data.materials}"
+      title.text = data.title
+      materials.text = data.materials
+      dimensions.text = data.dimensions
+      history.text = data.history
     }
   }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean =
+      when (item.itemId) {
+        android.R.id.home -> {
+          finish()
+          true
+        }
+
+        else -> super.onOptionsItemSelected(item)
+      }
 }
